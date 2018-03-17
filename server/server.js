e214cc77261c7442c249be50354c0b6c04a7de55
shivamarora123 <shivamarora@live.com>
2018-03-17 15:11:26 +0530

    First commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path=require('path');
+const publicPath=path.join(__dirname,'../public');
+const port=process.env.PORT||3000;
+const express=require('express');
+const app=express();
+app.use(express.static(publicPath));
+//For Starting the server
+app.listen(port,()=>{
+  console.log('Server started at port:'+port);
+});
