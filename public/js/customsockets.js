ff3cc35f593bea40617d384c6a2320d9a449b2bc
shivamarora123 <shivamarora@live.com>
2018-03-17 20:27:18 +0530

    broadcasting

---
+++ public/js/customsockets.js
@@ -0,0 +1,16 @@
+var socket=io();
+socket.on('connect',function(){
+ console.log('Connected');
+
+socket.emit('createMessage',{
+  from:'shivam',
+  text:'Howdy!'
+});
+socket.on('newMessage',function(data){
+  console.log('newMessage',data);
+});
+
+});
+socket.on('disconnect',function(){
+ console.log('DisConnected');
+});
--- server/server.js
+++ server/server.js
@@ -2,9 +2,38 @@ const path=require('path');
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
 const express=require('express');
+const socketIO=require('socket.io');
 const app=express();
+const http=require('http');
 app.use(express.static(publicPath));
+const server=http.createServer(app);
+//we are specifying server we are gonna use io sockets
+const io=socketIO(server);
+// connection is to check active connection and socket is passed in reference to var declared in html page
+io.on('connection',(socket)=>{
+ console.log('New User connected');
+
+//sending message to client side
+
+socket.on('createMessage',(message)=>{
+  console.log('createMessage',message);
+  io.emit('newMessage',{
+    from:message.from,
+    text:message.text,
+    createdAt:new Date().getTime()
+  });
+});
+
+
+//on disconnection from client
+socket.on('disconnect',()=>{
+  console.log("disconnected from client");
+});
+
+});
+
+
 //For Starting the server
-app.listen(port,()=>{
-  console.log('Server started at port:'+port);
+server.listen(port,()=>{
+  console.log('server is running at port:'+port);
 });
